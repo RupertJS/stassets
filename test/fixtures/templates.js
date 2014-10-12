a497baf6e637d905f869631972486ac2bdc19bb8
David Souther <davidsouther@gmail.com>
2014-10-12 14:22:37 -0400

    Test for HTML sourcemap.

--- test/fixtures/templates.js
+++ test/fixtures/templates.js
@@ -1,8 +1,6 @@
-angular.module('main.template', [])
+
+angular.module('main.login.html.template', [])
 .run(function($templateCache){
-    $templateCache.put('main', '<div class="main cascade"></div>');
+    $templateCache.put('main/login.html', '<div><form><input type="email" /></form></div>');
 });
-angular.module('main.nav.template', [])
-.run(function($templateCache){
-    $templateCache.put('main/nav', '<nav role="navigation"></nav>');
-});
+
